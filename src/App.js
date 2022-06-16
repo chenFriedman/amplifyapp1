e49f662fe1bb60b9d671dcbec8827145e66450b6
Chen Friedman <chen.friedman11@gmail.com>
2022-06-16 23:31:46 +0300

    aplify auth added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import './App.css';
+import "@aws-amplify/ui-react/styles.css";
+import { withAuthenticator, Button, View} from "@aws-amplify/ui-react";
+import  { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
 import BitcoinApp from './bitcoinApp/bitcoinApp';
+import './App.css';
+
+Auth.configure(awsconfig);
 
 function App() {
   return (
-    <div className="App">
+    <View className="App">
+      <Button className="signout" onClick={() => Auth.signOut()}>Sign Out</Button> {/* //to fix signout */}
       <BitcoinApp/>
-    </div>
+    </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);

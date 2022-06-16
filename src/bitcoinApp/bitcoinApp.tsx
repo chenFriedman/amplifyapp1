a68b068fe89e1e69ccb83dc52eb4020885202952
Chen Friedman <chen.friedman11@gmail.com>
2022-06-16 14:31:10 +0300

    get usd, ils bitcoin rates with apikey

--- src/bitcoinApp/bitcoinApp.tsx
+++ src/bitcoinApp/bitcoinApp.tsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
 import './style.scss'
 
+import axios from 'axios';
+
 import DatePicker from './datePicker/datePicker';
-import BitcoinChart from './bitcoinChart/bitcoinChart';
 import CoinButtons from  './coinButtons/coinButtons';
+import BitcoinChart from './bitcoinChart/bitcoinChart';
 import FavoriteButton from './favoriteButton/favoriteButton';
 
 const BitcoinApp = () => {
@@ -12,22 +14,32 @@ const BitcoinApp = () => {
     const [isFavorite, setIsFavorite] = useState <boolean>(false);
     const [values, setValues] = useState <(number | null)[]>([])
  
-    // QYGK2PVB327ONSO2
+    const apikey = 'QYGK2PVB327ONSO2'
+    const url = 'https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=BTC&market=ILS&apikey=$(apikey)';
+
+    useEffect(() => {
+        getBitCoinRates();
+    }, []);
+    
+    const getBitCoinRates = () => {
+        axios.get(url).then((res) => {
+            console.log(res.data['Time Series (Digital Currency Daily)']);
+        });
+    };
+    
     useEffect(() => {
         //get values from db according to new date
-        //if coin = shekel => calaulate    
     }, [date]);
 
     const updateSelectedCoin = () => {
         const coin = selectedCoin === 'USD' ? 'ILS' : 'USD'
         setSelectedCoin(coin);
-        // get money rate
         // change values according to money change
     }
 
     const updateIsFavorite = () => {
         setIsFavorite(!isFavorite)
-        //add to DB
+        //update value in DB
     }
 
     return (
